0f97ab511d91f62af5316367cffe27c865a465d3
Joseph Gaudard <jga051@bio3103914.klientdrift.uib.no>
2021-12-15 11:59:00 +0100

    snake_case!

--- R/ReadInTomstLogger.R
+++ R/ReadInTomstLogger.R
@@ -25,16 +25,16 @@ unzip("data/climate_tomst.zip", exdir = "data")
 # Read in meta data
 metaTomst <- read_csv2("data/logger_info.csv", col_names = TRUE, na = c(""), col_types = "fcffffncnc") %>% 
   mutate(
-    Date_logger_in = dmy(Date_logger_in),
-    Date_logger2_in = dmy(Date_logger2_in)
+    date_logger_in = dmy(date_logger_in),
+    date_logger2_in = dmy(date_logger2_in)
   ) %>% 
-  pivot_longer(cols = c("Tomst-logger","Tomst_logger_2"), names_to = "tomst", values_to = "loggerID") %>% #putting all the loggerID in one column and creating a new column specifying if it is logger 1 or 2
+  pivot_longer(cols = c("tomst-logger","tomst_logger_2"), names_to = "tomst", values_to = "loggerID") %>% #putting all the loggerID in one column and creating a new column specifying if it is logger 1 or 2
   mutate( #creating column date in and date out for selection of time window
-    date_in = case_when(tomst == "Tomst-logger" ~ Date_logger_in, 
-                        tomst == "Tomst_logger_2" ~ Date_logger2_in),
-    date_out = case_when(tomst == "Tomst-logger" ~ Date_logger2_in) #date ou of logger 1 is date in of logger 2, I guess it makes sense
+    date_in = case_when(tomst == "tomst-logger" ~ date_logger_in, 
+                        tomst == "tomst_logger_2" ~ date_logger2_in),
+    date_out = case_when(tomst == "tomst-logger" ~ date_logger2_in) #date ou of logger 1 is date in of logger 2, I guess it makes sense
   ) %>% 
-  select(!c(Date_logger_in, Date_logger2_in, tomst))
+  select(!c(date_logger_in, date_logger2_in, tomst))
 
 
 
@@ -58,8 +58,8 @@ temp <- map_df(set_names(files), function(file) {
 
 TomstLogger_2019_2020 <- temp %>% 
   # rename column names
-  rename("ID" = "X1", "Date_Time" = "X2", "Time_zone" = "X3", "SoilTemperature" = "X4", "GroundTemperature" = "X5", "AirTemperature" = "X6", "RawSoilmoisture" = "X7", "Shake" = "X8", "ErrorFlag" = "X9") %>% 
-  mutate(Date_Time = ymd_hm(Date_Time)) %>% 
+  rename("ID" = "X1", "datetime" = "X2", "time_zone" = "X3", "soil_temperature" = "X4", "ground_temperature" = "X5", "air_temperature" = "X6", "RawSoilmoisture" = "X7", "Shake" = "X8", "ErrorFlag" = "X9") %>% 
+  mutate(datetime = ymd_hm(datetime)) %>% 
   # Soil moisture calibration
   #mutate(SoilMoisture = a * RawSoilmoisture^2 + b * RawSoilmoisture + c) %>% 
   # get logger ID -> not needed anymore, have whole filename now!!!
@@ -96,26 +96,26 @@ TomstLogger_2019_2020 <- temp %>%
     date_out = replace_na(date_out, today("CET")) #the logger still in the field don't have a date_out (NA in the metaData), but we need a date_out to filter. Today's date can only be correct because if you are never running this script on future data ;-)
   ) %>% 
   filter(
-    Date_Time > date_in
-    & Date_Time <= date_out #maybe we don't have the data from the logger that broke, which is why are getting an empty df with that line
+    datetime > date_in
+    & datetime <= date_out #maybe we don't have the data from the logger that broke, which is why are getting an empty df with that line
   ) %>% 
   
   # fix wrong values (first graph them to see what to fix, below is what was done for another dataset)
-  mutate(AirTemperature = case_when(LoggerID %in% c("94194653") & AirTemperature < -40 ~ NA_real_,
-                                    TRUE ~ as.numeric(AirTemperature)),
+  mutate(air_temperature = case_when(LoggerID %in% c("94194653") & air_temperature < -40 ~ NA_real_,
+                                    TRUE ~ as.numeric(air_temperature)),
          
-         GroundTemperature = case_when(LoggerID %in% c("94194653") & GroundTemperature < -40 ~ NA_real_,
-                                       LoggerID %in% c("94194653") & GroundTemperature > 40 ~ NA_real_,
-                                       TRUE ~ as.numeric(GroundTemperature)),
+         ground_temperature = case_when(LoggerID %in% c("94194653") & ground_temperature < -40 ~ NA_real_,
+                                       LoggerID %in% c("94194653") & ground_temperature > 40 ~ NA_real_,
+                                       TRUE ~ as.numeric(ground_temperature)),
          
-         SoilTemperature = case_when(LoggerID %in% c("94194610", "94194613", "94194615", "94194616", "94194619", "94194652", "94194653", "94194654", "94194657", "94194658", "94194659", "94194660", "94194691", "94194694", "94194695", "94194699", "94205702", "94205731", "94205732") & SoilTemperature > 25 ~ NA_real_,
-                                     LoggerID %in% c("94194662", "94194667", "94194670", "94194693", "94194696", "94194611", "94194617", "94194626", "94194625", "94194655", "94194661", "94194681", "94194669", "94194666", "94205729", "94205727", "94205704", "94205703", "94205730", "94205731", "94205734", "94205760") & SoilTemperature > 20 ~ NA_real_,
-                                     LoggerID %in% c("94194653") & SoilTemperature < -40 ~ NA_real_,
+         soil_temperature = case_when(LoggerID %in% c("94194610", "94194613", "94194615", "94194616", "94194619", "94194652", "94194653", "94194654", "94194657", "94194658", "94194659", "94194660", "94194691", "94194694", "94194695", "94194699", "94205702", "94205731", "94205732") & soil_temperature > 25 ~ NA_real_,
+                                     LoggerID %in% c("94194662", "94194667", "94194670", "94194693", "94194696", "94194611", "94194617", "94194626", "94194625", "94194655", "94194661", "94194681", "94194669", "94194666", "94205729", "94205727", "94205704", "94205703", "94205730", "94205731", "94205734", "94205760") & soil_temperature > 20 ~ NA_real_,
+                                     LoggerID %in% c("94194653") & soil_temperature < -40 ~ NA_real_,
                                      # LoggerID %in% c("", "") & Date_Time > "2020-07-03 08:00:00" ~ NA_real_,
                                      # LoggerID %in% c("") & ErrorFlag == 1 ~ NA_real_,
                                      # LoggerID %in% c("") & Date_Time > "2020-07-17 01:00:00" & Date_Time < "2020-09-16 01:00:00" ~ NA_real_,
                                      # LoggerID == "" & Date_Time > "2020-08-12 00:00:00" & Date_Time < "2020-08-13 00:00:00" ~ NA_real_,
-                                     TRUE ~ as.numeric(SoilTemperature)))
+                                     TRUE ~ as.numeric(soil_temperature)))
 
 
 # Save clean file
