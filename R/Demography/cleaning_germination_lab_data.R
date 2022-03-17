fcb9a1f010c22dfee27add7009b65e57f69f6d60
Ragnhild Gya <Ragnhild.Gya@uib.no>
2022-03-17 18:28:30 +0100

    Adding comment corrections for Sibbaldia procumbens by using dictionaries

--- R/Demography/cleaning_germination_lab_data.R
+++ R/Demography/cleaning_germination_lab_data.R
@@ -29,12 +29,15 @@ get_file(node = "zhk3m",
 
 #### Load data ####
 
-Sib_pro_germ <- read_csv("data/Germination/INCLINE_Germination_Seedling_Experiment_Data_SP.csv")
+Sib_pro_germ <- read_csv2("data/Germination/INCLINE_Germination_Seedling_Experiment_Data_SP.csv")
 Ver_alp_germ <- read_csv2("data/Germination/INCLINE_Germination_Seedling_Experiment_Data_Va.csv")
 INCLINE_metadata <- read_delim("data/INCLINE_metadata_LoggerDates.csv", delim = ";")
-comment_dict <- read_delim("data/Germination/comment_dictionary.csv", delim = ";")
-harvest_comment_dict <- read_delim("data/Germination/harvest_comment_dictionary.csv", delim = ";")
-dish_comment_dict <- read_delim("data/Germination/dish_comment_dictionary.csv", delim = ";")
+comment_dict_VA <- read_delim("data/Germination/comment_dictionary_VA.csv", delim = ";")
+harvest_comment_dict_VA <- read_delim("data/Germination/harvest_comment_dictionary_VA.csv", delim = ";")
+dish_comment_dict_VA <- read_delim("data/Germination/dish_comment_dictionary_VA.csv", delim = ";")
+comment_dict_SP <- read_delim("data/Germination/comment_dictionary_SP.csv", delim = ";")
+harvest_comment_dict_SP <- read_delim("data/Germination/harvest_comment_dictionary_SP.csv", delim = ";")
+weighing_comment_dict_SP <- read_delim("data/Germination/weighing_comment_dictionary_SP.csv", delim = ";")
 
 ##### Veronica alpina #####
 
@@ -101,9 +104,9 @@ Ver_alp_germ <- Ver_alp_germ %>%
 Ver_alp_germ1 <- Ver_alp_germ %>% 
   mutate(flag_germination = case_when(flag %in% c("Duplicate_remove", "Remove_duplicate", "Remove_Duplicate", "Remove_duplicat", "Remove_unsureID", "	No germination date. 2 seeds.; seed#2 germinated with cotyledons 23.03.2020'", "Might have two versions of this, becasue this one was not crossed out (18.05.2020) Agar dried out, crossed out dish 07.05.2020") ~ "Remove_duplicate")) %>% #Change the few comments in the flag column to match the generalized flags
   mutate(flag_seedling = case_when(flag == "Remove_rotten" ~ "Dead_plant")) %>% #Change the few comments in the flag column to match the generalized flags
-  left_join(comment_dict, by = c("comment", "flag_germination", "flag_seedling")) %>% #Translate from the comment column via dictionary
-  left_join(harvest_comment_dict, by = c("harvest_comment", "flag_germination", "flag_seedling", "flag_whole_petridish")) %>% #translate from the harvest_comment column via dictionary
-  left_join(dish_comment_dict, by = c("comment", "flag_whole_petridish")) %>% #None of these comments apply to Ver_alp, so maybe they are comments for Sib_pro?
+  left_join(comment_dict_VA, by = c("comment", "flag_germination", "flag_seedling")) %>% #Translate from the comment column via dictionary
+  left_join(harvest_comment_dict_VA, by = c("harvest_comment", "flag_germination", "flag_seedling", "flag_whole_petridish")) %>% #translate from the harvest_comment column via dictionary
+  left_join(dish_comment_dict_VA, by = c("comment", "flag_whole_petridish")) %>% #None of these comments apply to Ver_alp, so maybe they are comments for Sib_pro?
   select(!flag)  %>% #Remove old flag column
   group_by(petri_dish) %>% 
   fill(flag_whole_petridish, .direction = "downup") #Give whole petridish comment too all seeds in the same petri dish
@@ -124,7 +127,6 @@ SP_mistakes <- read.table(header = TRUE, stringsAsFactors = FALSE, text =
 #Removing wrongly enetered data, fixing wrong dates and making new variables
 
 Sib_pro_germ1 <- Sib_pro_germ %>% 
-  mutate(Germination_date = ifelse(Germination_date %in% c("Agar dried out, crossed out dish 07.05.2022", "Agar dried out, crossed out dish 07.05.2021", "Agar dried out, crossed out dish 07.05.2020"), NA, Germination_date)) %>% 
   mutate(Germination_date = plyr::mapvalues(Germination_date, from = SP_mistakes$old, to = SP_mistakes$new)) %>% 
   mutate(Cotelydon_date = plyr::mapvalues(Cotelydon_date, from = SP_mistakes$old, to = SP_mistakes$new)) %>%
   mutate(Leaf_date = plyr::mapvalues(Leaf_date, from = SP_mistakes$old, to = SP_mistakes$new)) %>% 
@@ -142,8 +144,34 @@ Sib_pro_germ1 <- Sib_pro_germ %>%
   group_by(Species, Site, Water_potential, Replicate) %>% 
   mutate(seeds_in_dish = n()) %>% 
   ungroup() %>% 
-  select(!comment) %>% 
-  rename(species = Species, siteID = Site, water_potential = Water_potential, replicate = Replicate, seed_nr = Seed, start_date = Start_date, germination_date = Germination_date, comment = Comment...9, cotelydon_date = Cotelydon_date, leaf_date = Leaf_date, harvest_date = Harvest_date, harvest_comment = Harvest_comment, wet_mass_g_root = Wet_mass_g_root, wet_mass_g_above_ground = Wet_mass_g_rest, wet_mass_g_total = "Total wet mass", wet_mass_g_true_leaf = "Wet_mass_g_True leaf", dry_mass_g_root = Dry_mass_g_root, dry_mass_g_above_ground = Dry_mass_g_above_ground, dry_mass_g_total = Dry_mass_g_total, weighing_comments = Weighing_comments, removel_whole_petridish = Remove_whole_petridish, seed_viable = Viable_seeds, lights_off = "Lights_off (Yes/no)", flag = Flag)
+  rename(species = Species, siteID = Site, water_potential = Water_potential, replicate = Replicate, seed_nr = Seed, start_date = Start_date, germination_date = Germination_date, cotelydon_date = Cotelydon_date, leaf_date = Leaf_date, harvest_date = Harvest_date, wet_mass_g_root = Wet_mass_g_root, wet_mass_g_above_ground = Wet_mass_g_rest, wet_mass_g_total = "Total wet mass", wet_mass_g_true_leaf = "Wet_mass_g_True leaf", dry_mass_g_root = Dry_mass_g_root, dry_mass_g_above_ground = Dry_mass_g_above_ground, dry_mass_g_total = Dry_mass_g_total, seed_viable = Viable_seeds, lights_off = "Lights_off (Yes/no)")
 
 #### Deal with comments. Categorize them ####
 ## Entering information in flag columns from comment section. I have three columns, flags for the germination (when seeds rotted, or became sick, or when we believe there are mistakes in the dates), seedlings (when the plant has started rotting, or died before seedlings where harvested - to be used for filtering seedlings out of the final data set), and whole petri dish flags - when a shole petridish needs removing because of drying out or mold. Options for flags are: Remove_duplicate, Dead_plant, Sick_plant,  Missing_date, Possible_mistakes_in_ID, Biomass_mistakes, Moldy, Agar_issues and Other. Using dictionaries to translate between comments and flags.
+
+Sib_pro_germ2 <- Sib_pro_germ1 %>% 
+  # mutate(flag_germination = NA,
+  #        flag_seedling = NA,
+  #        flag_whole_petridish = NA) %>% 
+  left_join(comment_dict_SP, by = c("Comment")) %>% #Translate from the comment column via dictionary
+  left_join(harvest_comment_dict_SP, by = c("Harvest_comment")) %>% #translate from the harvest_comment column via dictionary
+  left_join(weighing_comment_dict_SP, by = c("Weighing_comments")) %>% #translate from the weighing_comment column via
+  rename(flag_germination = flag_germination.x, flag_seedling.z = flag_seedling, flag_seedling = flag_seedling.y) %>% 
+  mutate(flag_germination = case_when(is.na(flag_germination) ~ flag_germination.y,
+                                      !is.na(flag_germination) ~ flag_germination)) %>% 
+  mutate(flag_seedling = case_when(is.na(flag_seedling) ~ flag_seedling.x,
+                                   !is.na(flag_seedling) ~ flag_seedling)) %>% 
+  select(!flag_seedling.z & !flag_germination.y & !flag_seedling.x) %>% 
+  mutate(flag_germination = case_when(is.na(flag_germination) & Remove_whole_petridish == "Remove_duplicate" ~ "Remove_duplicate",
+                                      is.na(flag_germination) & Flag %in% c("Remove_duplicate", "Remove_flag") ~ "Remove_duplicate",
+                                      !is.na(flag_germination) ~ flag_germination)) %>%
+  mutate(flag_seedling = case_when(is.na(flag_seedling) & Remove_whole_petridish == "Remove_parts_missing" ~ "Biomass_mistakes",
+                                   is.na(flag_seedling) & Flag == "Remove_biomass_rotten" ~ "Biomass_mistakes",
+                                   !is.na(flag_seedling) ~flag_seedling)) %>% 
+  mutate(flag_whole_petridish = case_when(is.na(flag_whole_petridish) & Remove_whole_petridish %in% c("Dried out, remove", "dried out, remove") ~ "Agar_issues",
+                                          !is.na(flag_whole_petridish) ~ flag_whole_petridish)) %>% #Move the few comments in the flag and remove-whole-petridish column to new flag columns 
+  select(!Remove_whole_petridish & !Flag) %>% #Remove old flag columns
+  rename(comment = Comment, harvest_comment = Harvest_comment, weighing_comment = Weighing_comments) %>% 
+  group_by(petri_dish) %>% 
+  fill(flag_whole_petridish, .direction = "downup") #Give whole petridish comment too all seeds in the same petri dish
+
