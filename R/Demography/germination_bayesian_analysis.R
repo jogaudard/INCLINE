4588ab83916f6024a9de2f45fb7ca44123a8dcc6
Ragnhild Gya <ragnhild.gya@uib.no>
2022-04-04 11:33:45 +0200

    Trying a new model with Joachim (does not work yet)

--- R/Demography/germination_bayesian_analysis.R
+++ R/Demography/germination_bayesian_analysis.R
@@ -20,23 +20,26 @@ WP <- as.numeric(Ver_alp_germination_traits$water_potential)
 Precip <- as.numeric(Ver_alp_germination_traits$precip)
 
 # dependent variables
-GermN <- as.numeric(Ver_alp_germination_traits$n_germinated) #How is this supposed to be: a column with the number of seeds that did germinate? meaning it will be replicated for all the seeds in each petridish. Or 1 if it gerinated and 0 if it didn't?
+GermN <- as.numeric(Ver_alp_germination_traits$n_germinated)
 NumSeedDish <- as.numeric(Ver_alp_germination_traits$seeds_in_dish)  #Viability test information needs to be inculded here
 N <- as.numeric(length(GermN))
 
 treatmat <- model.matrix(~site*WP)
 
 jags.data <- list("site", "treatmat", "GermN", "N", "NumSeedDish")
-jags.param <- c("b", "prec1", "sig1", "rss", "rss_new")
+jags.param <- c("alpha", "beta", "rss", "rss_new") #, "prec1", "sig1"
 
 model_GermN <- function(){
   #group effects
-  for (j in 1:4){lokaliteter[j]~dnorm(0, prec1)}
+#  for (j in 1:4){lokaliteter[j]~dnorm(0, prec1)}
   
   #likelihood
   for (i in 1:N){
     GermN[i] ~ dbinom(mu[i], NumSeedDish[i])
-    logit(mu[i]) <- inprod(b, treatmat[i,])
+    
+    # linear predictor
+    logit(mu[i]) <- alpha[site[i]]+beta[site[i]]*WP
+      #logit(mu[i]) <- inprod(b, treatmat[i,])
     
     #posterior predictive checks (PPC)
     res[i] <- pow(GermN[i] - (mu[i]), 2)
@@ -45,9 +48,14 @@ model_GermN <- function(){
   }
   
   #priors
-  for(i in 1:8){b[i] ~ dnorm(0,1.0E-6)} #dnorm in JAGS uses mean and precision (0 = mean and 1.0E-6 = precision) different from dnorm in R that has variance and not precision.
-  prec1 ~ dgamma(0.001, 0.001) 
-  sig <- 1/sqrt(prec1) #getting variance of the random effect
+  for (i in 1:8){
+    alpha[i] ~ dnorm(0,0.01) # Intercepts
+    beta[i] ~ dnorm(0,0.01) # Slopes
+  }
+  
+  # for(i in 1:8){b[i] ~ dnorm(0,1.0E-6)} #dnorm in JAGS uses mean and precision (0 = mean and 1.0E-6 = precision) different from dnorm in R that has variance and not precision.
+  # prec1 ~ dgamma(0.001, 0.001) 
+  # sig <- 1/sqrt(prec1) #getting variance of the random effect
   
   #derived params
   rss <- sum(res[])
@@ -57,7 +65,7 @@ model_GermN <- function(){
 results_GermN <- jags.parallel(data = jags.data,
                                inits = NULL,
                                parameters.to.save = jags.param,
-                               n.iter = 50000,
+                               n.iter = 5000,
                                model.file = model_GermN,
                                n.thin = 5,
                                n.chains = 3)
