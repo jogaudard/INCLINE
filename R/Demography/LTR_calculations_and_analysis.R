5f1494ed04b085f69eccc0cb708a0ff90db649a7
Ragnhild Gya <ragnhild.gya@uib.no>
2022-04-21 14:00:47 +0200

    Updating and building LTR analysis for the difference in precipiation 1 and 2 for the control plots

--- R/Demography/LTR_calculations_and_analysis.R
+++ R/Demography/LTR_calculations_and_analysis.R
@@ -1,57 +1,67 @@
 ### LTREs
-# TT2 vs TT1sTT2
-BaseIPM <- (first_IPM+second_IPM)/2
+
+### CC - difference between precipitation levels ###
+
+#Making a matrix with the different between the two matrixes
+BaseIPM <- (IPM_VA_CC_precip1 + IPM_VA_CC_precip2) / 2
 SBaseIPM <- sens(BaseIPM)
-Difference.fec <- Fmatrix_VA_CR-Fmatrix_VA_CC
-Contributions.fec = Difference.fec*SBaseIPM
-sum(Contributions.fec)
 
-contourPlot(t(VO.TT2.Contributions.fec), c(1:dim(VO.TT2.Difference.fec)[1]), maxSize.VO.TT2, 0.01, -0.01)
-title("LTRE, TT1sTT2 vs. TT2, fec")
+#Calculating difference in fecundity
+Difference_fec <- Fmatrix_VA_CC_prec2 - Fmatrix_VA_CC_prec1
+Contributions_fec <- Difference_fec*SBaseIPM
+sum(Contributions_fec)
 
-Difference.clone <- Cmatrix_CR-Cmatrix_CC
-Contributions.clone = Difference.clone*SBaseIPM
-sum(Contributions.clone)
+contourPlot2(t(Contributions_fec), c(1:dim(Difference_fec)[1]), maxSize, 0, -0.0005, title = "LTRE fecundity Veronica alpina CC precip 1:precip2")
 
-contourPlot(t(VO.TT2.Contributions.clone), c(1:dim(VO.TT2.Difference.clone)[1]), maxSize.VO.TT2, 0.01, -0.01)
-title("LTRE, TT1sTT2 vs. TT2, clone")
+#Calculating difference in clonality
+Difference_clone <- Cmatrix_VA_CC_prec2 - Cmatrix_CC_prec1
+Contributions_clone = Difference_clone*SBaseIPM
+sum(Contributions_clone)
 
-Difference.pm <- Pmatrix_CR-Pmatrix_CC
-Contributions.pm = Difference.pm*SBaseIPM
-sum(Contributions.pm)
+contourPlot2(t(Contributions_clone), c(1:dim(Difference_clone)[1]), maxSize, 0, -0.0001, title = "LTRE clone Veronica alpina CC precip 1:precip2")
 
-contourPlot(t(VO.TT2.Contributions.pm), c(1:dim(VO.TT2.Difference.pm)[1]), maxSize.VO.TT2, 0.01, -0.01)
-title("LTRE, TT1sTT2 vs. TT2, Pmatrix")
+#Calculating difference in P matrix
+Difference_pm <- Pmatrix_VA_CC_precip2 - Pmatrix_VA_CC_precip1
+Contributions_pm = Difference_pm*SBaseIPM
+sum(Contributions_pm)
 
-sum(Contributions.fec)+sum(Contributions.clone)+sum(Contributions.pm)
+contourPlot2(t(Contributions_pm), c(1:dim(Difference_pm)[1]), maxSize, 0, -0.0002, title = "LTRE growth and survival VA CC precip 1:precip2")
 
-as.numeric(eigen(IPM.VO.TT2.all)$value[1])-as.numeric(eigen(IPM.VO.TT1sTT2.all)$value[1])
+#Adding together all the differences in vital rates to give a general difference between matrixes
+sum(Contributions_fec)+sum(Contributions_clone)+sum(Contributions_pm)
 
-growth.only <- coerceSurvObj(so_CR, c(100,0))
+#Find the difference in lamba between populations_
+as_numeric(eigen(IPM_VA_CC_precip2)$value[1])-as_numeric(eigen(IPM_VA_CC_precip1)$value[1])
 
-Pmatrix.growth.only <- makeIPMPmatrix(survObj=growth.only, growObj=go_CR, minSize=minSize, maxSize=maxSize, discreteTrans = dto_VA_CR, correction = "constant", nBigMatrix = 100)
+#Calculating contribution of growth alone
+growth_only_CC_precip1 <- coerceSurvObj(so_VA_CC, c(100,0))
+Pmatrix_growth_only_CC_precip1 <- makeIPMPmatrix(survObj=growth_only_CC_precip1, growObj=go_VA_CC_prec1, minSize=minSize, maxSize=maxSize, discreteTrans = dto_VA_CC, correction = "constant", nBigMatrix = 100)
 
 
-growth.only_CC <- coerceSurvObj(so_CC, c(100,0))
-Pmatrix.growth.only_CC <- makeIPMPmatrix(survObj=growth.only_CC, growObj=go_CC, minSize=minSize, maxSize=maxSize, discreteTrans = dto_VA_CC, correction = "constant", nBigMatrix = 100)
+growth_only_CC_precip2 <- coerceSurvObj(so_VA_CC, c(100,0))
+Pmatrix_growth_only_CC_precip2 <- makeIPMPmatrix(survObj=growth_only_CC_precip2, growObj=go_VA_CC_prec2, minSize=minSize, maxSize=maxSize, discreteTrans = dto_VA_CC, correction = "constant", nBigMatrix = 100)
 
-Difference.growth <- Pmatrix.growth.only-Pmatrix.growth.only_CC
+Difference_growth <- Pmatrix_growth_only_CC_precip2 - Pmatrix_growth_only_CC_precip1
 
-Contributions.growth = Difference.growth*SBaseIPM
+Contributions_growth = Difference_growth*SBaseIPM
+sum(Contributions_growth)
 
-sum(Contributions.growth)
-contourPlot(t(VO.TT2.Contributions.growth), c(1:dim(VO.TT2.Difference.growth)[1]), maxSize.VO.TT2, 0.006, -0.006)
-title("LTRE, TT1sTT2 vs. TT2, growth")
-# contributions survival
+contourPlot2(t(Contributions_growth), c(1:dim(Contributions_growth)[1]), maxSize, 0, -0.0003, "LTRE growth and survival Veronica alpina CC precip 1:precip2")
 
-Contributions.surv <- Contributions.pm-Contributions.growth       # or sum(VO.TT2.Contributions.pm)-sum(VO.TT2.Contributions.growth)
+# Calculating contribution of survival alone
+Contributions_surv <- Contributions_pm - Contributions_growth 
+contourPlot2(t(Contributions_surv), c(1:dim(Contributions_surv)[1]), maxSize, 0, -0.0003, "LTRE growth and survival Veronica alpina CC precip 1:precip2")
 
-sum(Contributions.fec)
-sum(Contributions.clone)
-sum(Contributions.growth)
-sum(Contributions.surv)
 
-sum(sum(Contributions.fec),
-    sum(Contributions.clone),
-    sum(Contributions.growth),
-    sum(Contributions.surv))
+sum(Contributions_fec)
+sum(Contributions_clone)
+sum(Contributions_growth)
+sum(Contributions_surv)
+
+sum(sum(Contributions_fec),
+    sum(Contributions_clone),
+    sum(Contributions_growth),
+    sum(Contributions_surv))
+
+
+as.numeric(eigen(IPM_VA_CC_precip2)$value[1])-as.numeric(eigen(IPM_VA_CC_precip1)$value[1])
