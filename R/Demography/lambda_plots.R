2fcf324f9b31c38ffe8de9d8624893fe357cf8a6
Ragnhild Gya <ragnhild.gya@uib.no>
2022-05-31 19:40:35 +0200

    Trying out different versions of the difference in lambda plots

--- R/Demography/lambda_plots.R
+++ R/Demography/lambda_plots.R
@@ -295,12 +295,273 @@ lambda_df_differences2 <- lambda_df_differences %>%
          CN_comparison = lambda - lambda_CN,
          WE_comparison = lambda - lambda_WE)
 
+lambda_df_differences3 <- lambda_df_differences2 %>% 
+  pivot_longer(cols = c(CC_comparison, WC_comparison, CR_comparison), values_to = "value")
 
-lambda_diff_CR_lolliplot <- lambda_df_differences2 %>% 
-  filter(Treatments == "WR") %>% 
+### Plots for prediction 0: The direct effects of warming have a positive effect on population dynamics, while the indirect effects counteract these positive effects.
+
+lambda_diff_VA_WR_CR_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Veronica alpina") %>% 
+  filter(Treatments %in% c("WR")) %>% 
   mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
-  ggplot(aes(y = CR_comparison, x = Species, color = precipitation)) +
-  geom_linerange(aes(ymin=0, ymax=CR_comparison, x=Species), position = position_dodge(width = 0.40))+
+  ggplot(aes(y = CR_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=CR_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("CR") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(-0.25, 0.8)
+
+lambda_diff_VA_WC_CC_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Veronica alpina") %>% 
+  filter(Treatments %in% c("WC")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = CC_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=CC_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("CC") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(-0.25, 0.8)
+
+
+lambda_diff_SP_WR_CR_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Sibbaldia procumbens") %>% 
+  filter(Treatments %in% c("WR")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = CR_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=CR_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("CR") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(-0.1, 0.1)
+
+lambda_diff_SP_WC_CC_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Sibbaldia procumbens") %>% 
+  filter(Treatments %in% c("WC")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = CC_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=CC_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("CC") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(-0.1, 0.1)
+
+P0_VA_plot <- ((lambda_diff_VA_WR_CR_lolliplot + lambda_diff_VA_WC_CC_lolliplot)) + 
+  plot_annotation( title = "Direct and indirect effects in the current alpine community",
+                   subtitle = 'Veronica alpina') +
+  plot_layout(widths = c(1,1), guides = 'collect') &
+  theme(legend.position = "bottom", text = element_text(size = 14))
+
+P0_SP_plot <- ((lambda_diff_SP_WR_CR_lolliplot + lambda_diff_SP_WC_CC_lolliplot)) + 
+  plot_annotation( title = "Direct and indirect effects in the current alpine community",
+                   subtitle = 'Sibbaldia procumbens') +
+  plot_layout(widths = c(1,1), guides = 'collect') &
+  theme(legend.position = "bottom", text = element_text(size = 14))
+
+### Plots for prediction 1: There are facilitative interactions between species in alpine plant communities that change to competitive when warmed.
+
+lambda_diff_VA_WR_WC_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Veronica alpina") %>% 
+  filter(Treatments %in% c("WR")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = WC_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=WC_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("WC") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(-0.25, 0.8)
+
+lambda_diff_VA_CR_CC_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Veronica alpina") %>% 
+  filter(Treatments %in% c("CR")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = CC_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=CC_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("CC") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(-0.25, 0.8)
+
+lambda_diff_SP_WR_WC_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Sibbaldia procumbens") %>% 
+  filter(Treatments %in% c("WR")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = WC_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=WC_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("WC") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(-0.1, 0.1)
+
+lambda_diff_SP_CR_CC_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Sibbaldia procumbens") %>% 
+  filter(Treatments %in% c("CR")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = CC_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=CC_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("CC") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(-0.1, 0.1)
+
+
+P1_VA_plot <- ((lambda_diff_VA_CR_CC_lolliplot + lambda_diff_VA_WR_WC_lolliplot)) + 
+  plot_annotation( title = "Changes in interactions within current plant communities",
+                   subtitle = 'Veronica alpina') +
+  plot_layout(widths = c(1,1), guides = 'collect') &
+  theme(legend.position = "bottom", text = element_text(size = 14))
+
+P1_SP_plot <- ((lambda_diff_SP_CR_CC_lolliplot + lambda_diff_SP_WR_WC_lolliplot)) + 
+  plot_annotation( title = "Changes in interactions within current plant communities",
+                   subtitle = 'Sibbaldia procumbens') +
+  plot_layout(widths = c(1,1), guides = 'collect') &
+  theme(legend.position = "bottom", text = element_text(size = 14))
+
+
+### Plots for prediction 2: The immigration of functionally novel species is more negative to the population dynamics of alpine species than the immigration of functionally similar species, and more so under warming. 
+
+lambda_diff_VA_CEN_CC_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Veronica alpina") %>% 
+  filter(Treatments %in% c("CE", "CN")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = CC_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=CC_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("CC") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(-0.25, 0.8)
+
+lambda_diff_VA_WEN_WC_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Veronica alpina") %>% 
+  filter(Treatments %in% c("WE", "WN")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = WC_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=WC_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("WC") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(-0.25, 0.8)
+
+lambda_diff_SP_CEN_CC_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Sibbaldia procumbens") %>% 
+  filter(Treatments %in% c("CE", "CN")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = CC_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=CC_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("CC") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(-0.1, 0.1)
+
+lambda_diff_SP_WEN_WC_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Sibbaldia procumbens") %>% 
+  filter(Treatments %in% c("WE", "WN")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = WC_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=WC_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("WC") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(-0.1, 0.1)
+
+P2_VA_plot <- ((lambda_diff_VA_CEN_CC_lolliplot + lambda_diff_VA_WEN_WC_lolliplot)) + 
+  plot_annotation( title = "Indirect effects via. novel interactions (extant vs. novel traits)",
+                   subtitle = 'Veronica alpina') +
+  plot_layout(widths = c(1,1), guides = 'collect') &
+  theme(legend.position = "bottom", text = element_text(size = 14))
+
+P2_SP_plot <- ((lambda_diff_SP_CEN_CC_lolliplot + lambda_diff_SP_WEN_WC_lolliplot)) + 
+  plot_annotation( title = "Indirect effects via. novel interactions (extant vs. novel traits)",
+                   subtitle = 'Sibbaldia procumbens') +
+  plot_layout(widths = c(1,1), guides = 'collect') &
+  theme(legend.position = "bottom", text = element_text(size = 14))
+
+
+#### Final plots with everything together
+
+  
+
+
+
+#######################
+
+lambda_diff_SP_R_CC_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Sibbaldia procumbens") %>% 
+  filter(Treatments %in% c("WR", "CR")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = CC_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=CC_comparison, x= Treatments), position = position_dodge(width = 0.40))+
   geom_point(size = 5, position = position_dodge(width = 0.40)) +
   #facet_wrap(~Species, nrow = 2, scales = "free") +
   geom_hline(yintercept = 0) +
@@ -309,8 +570,120 @@ lambda_diff_CR_lolliplot <- lambda_df_differences2 %>%
   theme_bw() +
   ylab("Δ λ") +
   xlab("") +
-  ggtitle("WR - CR") +
+  ggtitle("CC") +
   theme(plot.title = element_text(hjust = 0.5))
+
+lambda_diff_VA_WR_CR_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Veronica alpina") %>% 
+  filter(Treatments %in% c("WR")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = CR_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=CR_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_x_discrete(limits = rev) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("CR") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(-0.15, 1.0)
+
+lambda_diff_SP_WR_CR_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Sibbaldia procumbens") %>% 
+  filter(Treatments %in% c("WR")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = CR_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=CR_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_x_discrete(limits = rev) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("CR") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+lambda_diff_VA_WR_WC_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Veronica alpina") %>% 
+  filter(Treatments %in% c("WR")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = WC_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=WC_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_x_discrete(limits = rev) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("WC") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(-0.15, 1.0)
+
+lambda_diff_SP_WR_WC_lolliplot <- lambda_df_differences2 %>% 
+  filter(Species == "Sibbaldia procumbens") %>% 
+  filter(Treatments %in% c("WR")) %>% 
+  mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+  ggplot(aes(y = WC_comparison, x = Treatments, color = precipitation)) +
+  geom_linerange(aes(ymin=0, ymax=WC_comparison, x= Treatments), position = position_dodge(width = 0.40))+
+  geom_point(size = 5, position = position_dodge(width = 0.40)) +
+  #facet_wrap(~Species, nrow = 2, scales = "free") +
+  geom_hline(yintercept = 0) +
+  scale_x_discrete(limits = rev) +
+  scale_color_manual(values = precip_3_palette) +
+  theme_bw() +
+  ylab("Δ λ") +
+  xlab("") +
+  ggtitle("WC") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+((lambda_diff_VA_R_CC_lolliplot + lambda_diff_VA_WR_CR_lolliplot + lambda_diff_VA_WR_WC_lolliplot)) + 
+  plot_annotation( title = 'P1) There are facilitative interactions between species in \n alpine plant communities that change to competitive when warmed',
+    subtitle = 'Veronica alpina') +
+  plot_layout(widths = c(2,1,1), guides = 'collect') &
+  theme(legend.position = "bottom", text = element_text(size = 14))
+
+
+# lambda_diff_VA_R_C <- lambda_df_differences3 %>% 
+#   filter(Species == "Veronica alpina") %>% 
+#   filter(Treatments %in% c("WR", "WC")) %>% 
+#   mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+#   ggplot(aes(y = value, x = Treatments, color = precipitation)) +
+#   geom_linerange(aes(ymin=0, ymax=value, x= Treatments), position = position_dodge(width = 0.40))+
+#   geom_point(size = 5, position = position_dodge(width = 0.40)) +
+#   facet_wrap(~name, nrow = 1) +
+#   geom_hline(yintercept = 0) +
+#   scale_x_discrete(limits = rev) +
+#   scale_color_manual(values = precip_3_palette) +
+#   theme_bw() +
+#   ylab("Δ λ") +
+#   xlab("") +
+#   ggtitle("WC") +
+#   theme(plot.title = element_text(hjust = 0.5))
+
+
+# lambda_diff_VA_WR_WC_lolliplot <- lambda_df_differences2 %>% 
+#   filter(Treatments == "CR") %>% 
+#   mutate(precipitation = factor(precipitation, levels = c( "1.2 m/year", "2.3 m/year", "3.4 m/year", "Across precipitation"))) %>% 
+#   ggplot(aes(y = WC_comparison, x = Species, color = precipitation)) +
+#   geom_linerange(aes(ymin=0, ymax=WC_comparison, x=Species), position = position_dodge(width = 0.40))+
+#   geom_point(size = 5, position = position_dodge(width = 0.40)) +
+#   #facet_wrap(~Species, nrow = 2, scales = "free") +
+#   geom_hline(yintercept = 0) +
+#   scale_x_discrete(limits = rev) +
+#   scale_color_manual(values = precip_3_palette) +
+#   theme_bw() +
+#   ylab("Δ λ") +
+#   xlab("") +
+#   ggtitle("WR - CR") +
+#   theme(plot.title = element_text(hjust = 0.5))
 
 lambda_diff_WC_CC_lolliplot <- lambda_df_differences2 %>% 
   filter(Treatments == "WC") %>% 
