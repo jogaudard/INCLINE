3b2e3f7fa8b10a4b2f24a74435f6f18506555db4
Ragnhild Gya <ragnhild.gya@uib.no>
2022-05-31 21:24:40 +0200

    Changing the columns to correct category, saving the csv file, and commenting out the plotting code

--- R/Biomass/cleaning_biomass_removal.R
+++ R/Biomass/cleaning_biomass_removal.R
@@ -37,6 +37,14 @@ removal <- removal %>%
     rename(siteID = Site, year = Year, date_removal = Removal_date, date_sorting = Sorting_date, date_weighing = Weighing_date, recorder_removal = Removal_person, recorder_sorting = Sorting_person, recorder_weighing = Weighing_person, treatment = Treatment, comments = Comments, functional_group = FunctionalGroup) %>% 
   select(-Plot)
 
+#### Change the colums to correct category ####
+removal <- removal %>% 
+  ungroup() %>% 
+  mutate(year = as.numeric(year),
+         date_removal = dmy(date_removal),
+         date_sorting = dmy(date_sorting),
+         date_weighing = dmy(date_weighing))
+
 
 #### Calculate numbers for biomass information in manuscripts ####
 #Mean biomass removed per year
@@ -48,29 +56,33 @@ removal %>% group_by(year, siteID, plotID) %>% mutate(total_biomass = sum(value,
 #Mean biomass removed per functional group
 removal %>% group_by(functional_group, siteID, plotID) %>% mutate(total_biomass = sum(value, na.rm = TRUE)) %>% ungroup() %>% group_by(functional_group) %>% mutate(mean_biomass = mean(total_biomass)) %>%  ungroup() %>%  select(functional_group,  mean_biomass) %>% unique() 
 
+### Save data to upload as cleaned on OSF
+
+#write.csv(removal, file = "data/cleaned_data/INCLINE_biomass_removal.csv", row.names = FALSE)
+
 
 #### Plot data ####
 
-#### Color palette ####
-Brown_Green_full_palette <- c("#543005", "#8C510A", "#BF812D","#DFC27D", "#F6E8C3","#F5F5F5", "#C7EAE5","#80CDC1","#35978F","#01665E", "#003C30")
-Brown_Green_6_color_palette <- c("#8C510A", "#BF812D", "#F6E8C3","#C7EAE5","#80CDC1","#35978F","#01665E")
-cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00")
-cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-palette_with_white <- c("#a6611a", "#dfc27d", "#f5f5f5", "#80cdc1", "#018571")
-palette <- c("#a6611a", "#dfc27d", "#80cdc1", "#018571")
-
-
-#### Biomass removal plot ####
-
-plot1 <- removal %>% 
-  ggplot(aes(y = value, x = year, fill = functional_group))+
-  geom_bar(position = "stack", stat = "identity") +
-  facet_wrap(treatment ~ siteID, nrow = 2) +
-  scale_fill_manual(values= rev(Brown_Green_6_color_palette)) +
-  #ggtitle("Biomass removed") +
-  ylab("Biomass (g)") +
-  theme_minimal() +
-  theme(plot.title = element_text(hjust = 0.5, size = 40),
-        text = element_text(size = 35))
-
-ggsave(filename = "biomass_removal_year_treatment_site2.jpg", plot = plot1, width = 20, height = 12)
+# #### Color palette ####
+# Brown_Green_full_palette <- c("#543005", "#8C510A", "#BF812D","#DFC27D", "#F6E8C3","#F5F5F5", "#C7EAE5","#80CDC1","#35978F","#01665E", "#003C30")
+# Brown_Green_6_color_palette <- c("#8C510A", "#BF812D", "#F6E8C3","#C7EAE5","#80CDC1","#35978F","#01665E")
+# cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00")
+# cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+# palette_with_white <- c("#a6611a", "#dfc27d", "#f5f5f5", "#80cdc1", "#018571")
+# palette <- c("#a6611a", "#dfc27d", "#80cdc1", "#018571")
+# 
+# 
+# #### Biomass removal plot ####
+# 
+# plot1 <- removal %>% 
+#   ggplot(aes(y = value, x = year, fill = functional_group))+
+#   geom_bar(position = "stack", stat = "identity") +
+#   facet_wrap(treatment ~ siteID, nrow = 2) +
+#   scale_fill_manual(values= rev(Brown_Green_6_color_palette)) +
+#   #ggtitle("Biomass removed") +
+#   ylab("Biomass (g)") +
+#   theme_minimal() +
+#   theme(plot.title = element_text(hjust = 0.5, size = 40),
+#         text = element_text(size = 35))
+# 
+# ggsave(filename = "biomass_removal_year_treatment_site2.jpg", plot = plot1, width = 20, height = 12)
