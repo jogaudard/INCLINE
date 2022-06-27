f573aa25484559bdbd562cc369c2760977217578
Joseph Gaudard <jga051@bio3103914.klientdrift.uib.no>
2022-06-27 14:19:16 +0200

    adapted for fieldwork

--- R/C-Flux/create_meta_field.R
+++ R/C-Flux/create_meta_field.R
@@ -7,7 +7,12 @@ get_file(node = "zhk3m",
          remote_path = "RawData")
 
 meta <- read_csv2("data/INCLINE_metadata.csv") %>% 
-  filter(treatment != "R") %>% 
-  select(turfID)
+  # filter(treatment != "R") %>% #remove R (removal) treatment for c-fluxes
+  slice(rep(1:n(), each = 4)) %>% #c-flux 2 replicates, NDVI 3, soil moisture 4
+  mutate(
+    # turfID = paste(turfID, OTC, treatment, sep = "_") #for printing sheet, turfID includes the treatment for easier reading
+    treatment = paste(OTC, treatment, sep = "_")
+  ) %>% 
+  select(turfID, treatment)
 
 write_csv(meta,"meta_field.csv")
