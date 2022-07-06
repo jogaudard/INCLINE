978554b1a0dccc1a4e075682d783266c1c01778d
Joseph Gaudard <joseph.gaudard@uib.no>
2022-07-06 10:03:54 +0200

    changed columns for printed data sheet

--- R/C-Flux/create_meta_field.R
+++ R/C-Flux/create_meta_field.R
@@ -8,11 +8,12 @@ get_file(node = "zhk3m",
 
 meta <- read_csv2("data/INCLINE_metadata.csv") %>% 
   # filter(treatment != "R") %>% #remove R (removal) treatment for c-fluxes
-  slice(rep(1:n(), each = 4)) %>% #c-flux 2 replicates, NDVI 3, soil moisture 4
+  slice(rep(1:n(), each = 1)) %>% #c-flux 2 replicates, NDVI 3, soil moisture 4
   mutate(
-    # turfID = paste(turfID, OTC, treatment, sep = "_") #for printing sheet, turfID includes the treatment for easier reading
-    treatment = paste(OTC, treatment, sep = "_")
+    turfID = paste(turfID, OTC, treatment, sep = "_") #for printing sheet, turfID includes the treatment for easier reading
+    # treatment = paste(OTC, treatment, sep = "_")
   ) %>% 
-  select(turfID, treatment)
+  # select(turfID, treatment)
+  select(turfID)
 
 write_csv(meta,"meta_field.csv")
