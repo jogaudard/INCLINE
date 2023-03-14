b92306987c73f6f5d7e21537596df295328f1974
Joseph Gaudard <joseph.gaudard@uib.no>
2023-03-14 16:57:53 +0100

    changed unit of fluxes and cover data (now all is correct and it was submitted to itex)

--- R/C-Flux/itex2023.R
+++ R/C-Flux/itex2023.R
@@ -48,7 +48,7 @@ fluxes_itex <- fluxes %>%
       siteID == "Ulvehaugen" ~ "NOR_10",
       siteID == "Skjellingahaugen" ~ "NOR_12"
     ),
-    flux = flux * 44 * 24 # they want mg CO2 / m2 / day
+    flux = flux * 0.04401 # they want g CO2 / m2 / h
   ) %>% 
   select(plotID, siteID, date, OTC, temp_soilavg, flux, temp_airavg, ITEX_ID, soil_moisture, comments, RMSE) %>% 
   rename(
@@ -75,20 +75,20 @@ write_csv(fluxes_itex, "ITEX/ITEX_fluxes_2022.csv")
 
 
 
-get_file(node = "zhk3m",
-         file = "community_clean_med_NA.csv",
-         path = "data_cleaned",
-         remote_path = "Community")
+# get_file(node = "zhk3m",
+#          file = "community_clean_med_NA.csv",
+#          path = "data_cleaned",
+#          remote_path = "Community")
+# 
+# get_file(node = "zhk3m",
+#          file = "INCLINE_community_2018_2019_2021_2022.csv",
+#          path = "data_cleaned",
+#          remote_path = "RawData/Community")
 
-get_file(node = "zhk3m",
-         file = "INCLINE_community_2018_2019_2021_2022.csv",
-         path = "data_cleaned",
-         remote_path = "RawData/Community")
-
-get_file(node = "zhk3m",
-         file = "community_clean_done_without_NA.csv",
-         path = "data_cleaned",
-         remote_path = "Community")
+# get_file(node = "zhk3m",
+#          file = "community_clean_done_without_NA.csv",
+#          path = "data_cleaned",
+#          remote_path = "Community")
 
 get_file(node = "zhk3m",
          file = "INCLINE_community_plotlevel_info.csv",
@@ -132,25 +132,33 @@ plot_data <- community %>%
     treatment == "C"
   ) %>% 
   mutate(
-    # lichen = replace_na(lichen, 0),
-    # moss = replace_na(moss, 0),
-    # forbs = Lot_cor + Aco_sep + Eri_sp + Eri_uni + Equ_sci + Equ_arv + Pin_vul + Tof_pus + Sau_alp + Leu_vul + Ant_odo + Eup_wet + Sib_pro + Alc_alp + Alc_sp + Oma_sup + Ver_alp + Vio_pal + Cam_rot + Sag_sag + Leo_aut + Sel_sel + Pyr_sp + Luz_mul + Tar_sp + Pot_cra + Dip_alp + Tha_alp + Lys_eur + Hie_alp + Rum_ace + Cer_cer + Epi_ana + Equ_arv + Epi_sp + Tof_pus + Nid_seedling + Bar_alp + Sil_aca + Par_pal + Hie_alp + Cer_fon + Pot_ere + Vio_bif + Coel_vir + Ran_acr + Gen_niv + Pin_vul + Eri_sp + Ach_mil + Pyr_min + Bis_viv + Ast_alp + Rum_acl + Bot_lun + Gen_ama + Ran_sp + Oxy_dig + Fern + Ger_syl + Geu_riv + Rhi_min + Hie_sp + Tri_ces + Hyp_sel + Sol_vir + Vio_can + Ort_sec + Pru_vul + Ver_off + Suc_pra + Hyp_mac + Ran_pyg + Dry_oct + Luz_spi + Tri_rep + Hyp_sp + Ste_gra + Sel_sp + Vio_tri + Ver_cha + Nid_juvenile + Gen_sp + Tri_sp + Oma_sp + Cer_alp + Tri_pra + Sil_vul + Sag_sp + Phe_con + Gym_dry + Oma_nor + Gal_sp + Gen_cam + Oxa_ace + Lot_cor + Aco_sep + Eri_uni + Equ_sci + Sau_alp + Leu_vul,
-    # graminoids = Nar_str + Agr_mer + Agr_cap + Car_big + Car_nor + Car_cap + Car_pal + Car_pil + Poa_pra + Car_vag + Ave_fle + Des_ces + Poa_alp + Jun_tri + Phl_alp + Fes_ovi + Fes_rub + Sau_alp + Fes_sp + Car_sp + Ant_dio + Fes_viv + Des_alp + Car_fla + Car_sax + Ant_sp + Car_atr,
-    # evergreens = Emp_nig + Vac_vit + Cal_vul,
-    # deciduous = Sal_her + Vac_myr + Vac_uli + Sal_sp + Bet_nan + Bet_pub + Sal_lan,
-    OTC = str_replace_all(warming, c("C" = "CTL", "W" = "OTC")),
-    ITEX_ID = case_when(
-      site == "Lavisdalen" ~ "NOR_9",
-      site == "Gudmedalen" ~ "NOR_11",
-      site == "Ulvehaugen" ~ "NOR_10",
-      site == "Skjellingahaugen" ~ "NOR_12"
+    functional_group = case_when(
+      species == "Hie_pil" ~ "Forbs",
+      TRUE ~ functional_group
     )
-  ) %>% 
-  select(site, plotID, OTC, functional_group, cover) %>% 
-  group_by(site, plotID, OTC, functional_group) %>% 
+  #   # lichen = replace_na(lichen, 0),
+  #   # moss = replace_na(moss, 0),
+  #   # forbs = Lot_cor + Aco_sep + Eri_sp + Eri_uni + Equ_sci + Equ_arv + Pin_vul + Tof_pus + Sau_alp + Leu_vul + Ant_odo + Eup_wet + Sib_pro + Alc_alp + Alc_sp + Oma_sup + Ver_alp + Vio_pal + Cam_rot + Sag_sag + Leo_aut + Sel_sel + Pyr_sp + Luz_mul + Tar_sp + Pot_cra + Dip_alp + Tha_alp + Lys_eur + Hie_alp + Rum_ace + Cer_cer + Epi_ana + Equ_arv + Epi_sp + Tof_pus + Nid_seedling + Bar_alp + Sil_aca + Par_pal + Hie_alp + Cer_fon + Pot_ere + Vio_bif + Coel_vir + Ran_acr + Gen_niv + Pin_vul + Eri_sp + Ach_mil + Pyr_min + Bis_viv + Ast_alp + Rum_acl + Bot_lun + Gen_ama + Ran_sp + Oxy_dig + Fern + Ger_syl + Geu_riv + Rhi_min + Hie_sp + Tri_ces + Hyp_sel + Sol_vir + Vio_can + Ort_sec + Pru_vul + Ver_off + Suc_pra + Hyp_mac + Ran_pyg + Dry_oct + Luz_spi + Tri_rep + Hyp_sp + Ste_gra + Sel_sp + Vio_tri + Ver_cha + Nid_juvenile + Gen_sp + Tri_sp + Oma_sp + Cer_alp + Tri_pra + Sil_vul + Sag_sp + Phe_con + Gym_dry + Oma_nor + Gal_sp + Gen_cam + Oxa_ace + Lot_cor + Aco_sep + Eri_uni + Equ_sci + Sau_alp + Leu_vul,
+  #   # graminoids = Nar_str + Agr_mer + Agr_cap + Car_big + Car_nor + Car_cap + Car_pal + Car_pil + Poa_pra + Car_vag + Ave_fle + Des_ces + Poa_alp + Jun_tri + Phl_alp + Fes_ovi + Fes_rub + Sau_alp + Fes_sp + Car_sp + Ant_dio + Fes_viv + Des_alp + Car_fla + Car_sax + Ant_sp + Car_atr,
+  #   # evergreens = Emp_nig + Vac_vit + Cal_vul,
+  #   # deciduous = Sal_her + Vac_myr + Vac_uli + Sal_sp + Bet_nan + Bet_pub + Sal_lan,
+  #   # OTC = str_replace_all(warming, c("C" = "CTL", "W" = "OTC")),
+  #   ITEX_ID = case_when(
+  #     site == "Lavisdalen" ~ "NOR_9",
+  #     site == "Gudmedalen" ~ "NOR_11",
+  #     site == "Ulvehaugen" ~ "NOR_10",
+  #     site == "Skjellingahaugen" ~ "NOR_12"
+  #   )
+  ) %>%
+  select(plotID, functional_group, cover) %>% 
+  group_by(plotID, functional_group) %>% 
   summarise(
     group_cover = sum(cover)
   ) %>% 
+  ungroup() %>%
+  pivot_wider(names_from = functional_group, values_from = group_cover) %>% 
+  left_join(INCLINE_metadata) %>%
+  left_join(height) %>% 
   # select(year, moss, lichen, Veg_height_mm, plotID, site, forbs, graminoids, evergreens, deciduous, OTC, ITEX_ID, coordinate_N, coordinate_E) %>% 
   # distinct() %>% 
   # group_by(year, plotID, site, OTC, ITEX_ID, coordinate_N, coordinate_E) %>% 
@@ -169,14 +177,36 @@ plot_data <- community %>%
     N_Coord = gsub("(\\d{2})(?=\\d{2})", "\\1 ", (coordinate_N/10), perl = TRUE),
     E_Coord = gsub("(^\\d)(?=\\d{2})(\\d{2})", "\\1 \\2 ", (coordinate_E/10), perl = TRUE),
     N_Coord = conv_unit(N_Coord, from = "deg_min_sec", to = "dec_deg"),
-    E_Coord = conv_unit(E_Coord, from = "deg_min_sec", to = "dec_deg")
+    E_Coord = conv_unit(E_Coord, from = "deg_min_sec", to = "dec_deg"),
+    OTC = str_replace_all(OTC, c("C" = "CTL", "W" = "OTC")),
+    community_mean_plant_height = vegetation_height_mean / 10, # they want cm
+    # ITEX_ID = case_when(
+    #   siteID == "Lavisdalen" ~ "NOR_9",
+    #   siteID == "Gudmedalen" ~ "NOR_11",
+    #   siteID == "Ulvehaugen" ~ "NOR_10",
+    #   siteID == "Skjellingahaugen" ~ "NOR_12"
+    #   )
+    ITEX_ID = str_replace_all(
+      siteID,
+      c(
+        "Lavisdalen" = "NOR_9",
+        "Gudmedalen" = "NOR_11",
+        "Ulvehaugen" = "NOR_10",
+        "Skjellingahaugen" = "NOR_12"
+      )
+    )
   ) %>% 
-  select(!c(coordinate_N, coordinate_E)) %>%
-  # left_join(soil_moisture_avg, by = c("plotID" = "turfID", "year")) %>% 
+  select(plotID, Deciduous_shrubs, Forbs, Graminoids, Evergreen_shrubs, ITEX_ID, OTC, total_bryophyte_cover, total_lichen_cover, community_mean_plant_height, N_Coord, E_Coord) %>%
   rename(
-    treatment = "OTC"
-  ) %>% 
-  arrange(site)
+    treatment = "OTC",
+    # community_mean_plant_height = "vegetation_height_mean",
+    Mosses = "total_bryophyte_cover",
+    Lichens = "total_lichen_cover"
+  ) %>%
+  relocate(ITEX_ID, treatment, plotID, N_Coord, E_Coord, community_mean_plant_height, Graminoids, Forbs, Deciduous_shrubs, Evergreen_shrubs, Mosses, Lichens) %>% 
+  # left_join(soil_moisture_avg, by = c("plotID" = "turfID", "year")) %>% 
+
+  arrange(ITEX_ID)
   
 write_csv(plot_data, "ITEX/ITEX_plot-data_2022.csv")
 
