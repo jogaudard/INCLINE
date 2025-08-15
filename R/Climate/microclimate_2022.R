d1846d87d6572042520bd924d6a259942b449029
jogaudard <joseph.gaudard@pm.me>
2025-08-15 11:32:09 +0200

    importing

--- R/Climate/microclimate_2022.R
+++ R/Climate/microclimate_2022.R
@@ -10,6 +10,22 @@ lapply(my_packages, library, character.only = TRUE)
 
 #### importing and reading data ####
 
+microclimate2022_og <- tomst_import_many(
+  "zhk3m",
+  c(
+    "INCLINE_TOMST_Fall2022.zip",
+    "INCLINE_TOMST_Spring2022.zip",
+    "TOMST_received_from_TOMST_fall2023.zip"
+  ),
+  "data",
+  "RawData/Climate"
+)
 
+microclimate2022 <- microclimate2022_og |>
+  filter(
+    year(datetime) == 2022
+  )
 
+tomst_metadata2022 <- read_csv("data/tomst_logger_metadata_2022.csv")
 
+# slicing ####
