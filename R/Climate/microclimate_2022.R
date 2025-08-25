6ac1522e4ffe0597dfc227a81d85a00d33edf36d
jogaudard <joseph.gaudard@pm.me>
2025-08-25 11:54:48 +0200

    found the missing data

--- R/Climate/microclimate_2022.R
+++ R/Climate/microclimate_2022.R
@@ -17,7 +17,10 @@ microclimate2022_og <- tomst_import_many(
   c(
     "INCLINE_TOMST_Fall2022.zip",
     "INCLINE_TOMST_Spring2022.zip",
-    "TOMST_received_from_TOMST_fall2023.zip"
+    "TOMST_received_from_TOMST_fall2023.zip",
+    "INCLINE_TOMST_Fall_2023.zip",
+    "INCLINE_microclimate.zip",
+    "INCLINE_TOMST_Spring_2023.zip"
   ),
   "data",
   "RawData/Climate"
@@ -53,10 +56,12 @@ microclimate2022 <- microclimate2022_all |>
     tomst_metadata2022, by = "loggerID", relationship = "many-to-many"
   ) |>
   filter(
-    datetime > date_in
-    & datetime < date_out
+    date(datetime) > date_in
+    & date(datetime) < date_out
   ) |>
   arrange(datetime)
+
+  
 
 # calculating soil moisture ####
 source("https://raw.githubusercontent.com/audhalbritter/Three-D/master/R/functions/soilmoisture_correction.R")
@@ -69,16 +74,74 @@ microclimate2022 <- microclimate2022 |>
       soilclass = "silt_loam" #it is the closest soil class, but still very wrong. The TMS calibration tool does not have any class for our soil
     ))
 
+microclimate2022 <- microclimate2022 |>
+  pivot_longer(c(soil_temperature, soil_moisture, ground_temperature, air_temperature), names_to = "sensor")
+
+# saveRDS(microclimate2022, "data/microclimate2022.rds")
+
+# microclimate2022 <- readRDS("data/microclimate2022.rds")
+
+# cleaning ####
+
+microclimate2022_clean <- microclimate2022 |>
+  mutate(value = case_when(
+    loggerID == 94205721 & date(datetime) == ymd("2022-02-18") ~ NA, # messed up and no record the next 3 days
+    siteID == "Gudmedalen" & month(datetime) == 8 & sensor == "soil_temperature" & value > 15 ~ NA,
+    .default = value
+  )
+  )
+
 # plottting ####
 
 
-microclimate2022 |>
+microclimate2022_clean |>
   filter(
-    datetime > ymd("2022-08-01")
-    & datetime < ymd("2022-08-31")
+    # datetime > ymd("2022-08-06")
+    # & datetime < ymd("2022-08-10")
+    month(datetime) == 10
+    # date(datetime) == ymd("2022-02-17")
     & siteID == "Gudmedalen"
+    # & loggerID == 94205721
   ) |>
-  pivot_longer(c(soil_temperature, soil_moisture, ground_temperature, air_temperature), names_to = "sensor") |>
   ggplot(aes(datetime, value)) +
   geom_point() +
   facet_wrap(. ~ sensor, scales = "free", ncol = 1)
+
+
+
+# checking ranges ####
+
+microclimate2022_clean |>
+  reframe(
+    .by = sensor,
+    range = range(value, na.rm = TRUE)
+  )
+
+microclimate2022_clean |>
+  filter(
+    sensor == "soil_temperature"
+    & value > 20
+    ) |>
+  View()
+
+microclimate2022_clean |>
+  filter(
+    siteID == "Gudmedalen"
+    & month(datetime) == 11) |>
+  View()
+
+microclimate2022_all |>
+  left_join(
+    tomst_metadata2022, by = "loggerID", relationship = "many-to-many"
+  ) |>
+  filter(
+    siteID == "Gudmedalen"
+    & month(datetime) == 11) |>
+  View()
+
+tomst_metadata2022 |>
+  filter(
+    season == "winter"
+    & siteID == "Gudmedalen"
+  ) |>
+  View()
